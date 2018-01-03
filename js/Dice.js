a9ad943cfdc61b6a676e4c814e8b0f60b226038a
Micro <maglars2@gmail.com>
2018-01-03 21:57:08 +0100

    Some ships and fleet GUI

---
+++ js/Dice.js
@@ -0,0 +1,5 @@
+class Dice {
+	static roll() {
+		return Math.floor(6 * Math.random()) + 1;
+	}
+}
--- js/GameController.js
+++ js/GameController.js
@@ -1,5 +1,35 @@
 class GameController {
 	constructor() {
 		this._gameBoard = new GameBoard();
+		this._createShips();
+	}
+
+	_createShips() {
+		const spain = [{'color': '#ffffff', 'x': 7, 'y': 0},
+					   {'color': '#ffffff', 'x': 8, 'y': 0},
+					   {'color': '#ffffff', 'x': 9, 'y': 0}
+					  ];
+		const arabia = [{'color': '#7E0CCA', 'x': 0, 'y': 7},
+			   			{'color': '#7E0CCA', 'x': 0, 'y': 8},
+			   			{'color': '#7E0CCA', 'x': 0, 'y': 9}
+			  		   ];
+		const france = [{'color': '#088362', 'x': 19, 'y': 10},
+			   			{'color': '#088362', 'x': 19, 'y': 11},
+			   			{'color': '#088362', 'x': 19, 'y': 12}
+			  		   ];
+		const pirates = [{'color': '#000000', 'x': 10, 'y': 19},
+			   			 {'color': '#000000', 'x': 11, 'y': 19},
+			   			 {'color': '#000000', 'x': 12, 'y': 19}
+			  		    ];
+
+		this._fleets = [spain, arabia, france, pirates];
+	}
+
+	getGameBoardGrid() {
+		return this._gameBoard.getGrid();
+	}
+
+	getFleets() {
+		return this._fleets;
 	}
 }
